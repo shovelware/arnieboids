3526f1894a4d7be02598bb99d80cbc0156659eb4
Alex Meuer <alexmeuer@gmail.com>
2015-12-04 12:20:06 +0000

    Changed toolset to v120. Added library dependencies in main.cpp

--- Arnieboids/main.cpp
+++ Arnieboids/main.cpp
@@ -1,3 +1,17 @@
+#if _DEBUG
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-main-d.lib")
+#pragma comment(lib, "sfml-network-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#else
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-main.lib")
+#pragma comment(lib, "sfml-network.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#endif
+
 int main()
 {
 
